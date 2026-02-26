69fb8435b6120045c52165674a4df0656e79d50f
sedoll <do11anm@naver.com>
2026-02-26 19:05:28 +0900

    260226 - footer, year 수정

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -48,7 +48,7 @@
 
     <footer class="bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 mt-auto">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <p class="text-center text-sm text-gray-500 dark:text-gray-400">© 2024 TrafficEye System. All rights reserved.</p>
+        <p class="text-center text-sm text-gray-500 dark:text-gray-400">© 2026 TrafficEye System. All rights reserved.</p>
       </div>
     </footer>
   </div>
